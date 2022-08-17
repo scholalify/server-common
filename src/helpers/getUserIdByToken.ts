273240499a0b8cb0aee4cb30f2262a2f8380c314
Dilum <dilum14@gmail.com>
2022-08-17 12:56:22 -0230

    Updates

--- src/helpers/getUserIdByToken.ts
+++ src/helpers/getUserIdByToken.ts
@@ -1,16 +1,16 @@
 import jwt_decode from "jwt-decode";
-const getUserIdByToken = (event: any) => {
+const getUserInfoByToken = (event: any) => {
   if (event.headers) {
     const token = event.headers.authorization;
     if (token) {
       const decoded = jwt_decode(token);
       if (decoded) {
         // @ts-ignore
-        return decoded.sub;
+        return decoded;
       }
     }
   }
   return null;
 };
 
-export default getUserIdByToken;
+export default getUserInfoByToken;
