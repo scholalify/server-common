7717f160ab1415e7da0cd70d66de11f00388a8fb
Dilum <dilum14@gmail.com>
2022-08-19 11:27:22 -0230

    Updates

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export * from "../../../server/common/src/errors/bad-request-error";
+export * from "./errors/bad-request-error";
 export * from "./errors/custom-error";
 export * from "./errors/database-connection-error";
 export * from "./errors/not-authorized-error";
